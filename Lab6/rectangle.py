b4b38b81d1fe5b22bbcf90dba9ee885f2269a610
Legion <pawel.kapusta@student.uj.edu.pl>
2020-11-13 22:41:21 +0100

    finish task2

---
+++ Lab6/rectangle.py
@@ -0,0 +1,31 @@
+from Lab6.points import Point
+import math
+
+
+class Rectangle:
+
+    def __init__(self, x1, y1, x2, y2):
+        self.pt1 = Point(x1, y1)
+        self.pt2 = Point(x2, y2)
+
+    def __str__(self):
+        return "({0},{1},{2},{3})".format(self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y)
+
+    def __repr__(self):
+        return "Rectangle({0},{1},{2},{3})".format(self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y)
+
+    def __eq__(self, other):
+        return self.pt1.x == other.pt1.x and self.pt1.y == other.pt1.y and self.pt2.x == other.pt2.x \
+               and self.pt2.y == other.pt2.y
+
+    def __ne__(self, other):
+        return not self == other
+
+    def center(self):
+        return Point((self.pt1.x + self.pt2.x) / 2, (self.pt1.y + self.pt2.y) / 2)
+
+    def area(self):
+        return math.pow(self.pt2.x - self.pt1.x, 2) + math.pow(self.pt2.y - self.pt1.y, 2)
+
+    def move(self, x, y):
+        return Rectangle(self.pt1.x + x, self.pt1.y + y, self.pt2.x + x, self.pt2.y + y)
---
+++ Lab6/task3.py
@@ -0,0 +1,35 @@
+from Lab6.rectangle import *
+import unittest
+
+
+class TestRectangle(unittest.TestCase):
+
+    def setUp(self):
+        self.rectangle = Rectangle(0, 0, 3, 4)
+
+    def test_str(self):
+        self.assertEqual(str(self.rectangle), "(0,0,3,4)")
+
+    def test_repr(self):
+        self.assertEqual(repr(self.rectangle), "Rectangle(0,0,3,4)")
+
+    def test_eq(self):
+        self.assertTrue(self.rectangle == Rectangle(0, 0, 3, 4))
+        self.assertTrue(self.rectangle != Rectangle(4, 5, 15, 15))
+
+    def test_center(self):
+        self.assertTrue(Rectangle.center(self.rectangle), Point(0, 3))
+
+    def test_area(self):
+        self.assertEqual(Rectangle.area(self.rectangle), 25)
+
+    def test_move(self):
+        self.assertEqual(Rectangle.move(self.rectangle, 1, 3), Rectangle(1, 3, 4, 7))
+        self.assertEqual(Rectangle.move(self.rectangle, -2, 5), Rectangle(-2, 5, 1, 9))
+
+    def tearDown(self):
+        del self.rectangle
+
+
+if __name__ == '__main__':
+    unittest.main()
